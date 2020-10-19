b6cff290f01097c5c9f131daa073eff69636a85a
joelvaz99 <joelvaz@ipvc.pt>
2020-10-19 15:45:45 +0100

    Create the DAO

---
+++ app/src/main/java/ipvc/estg/room/dao/CityDao.kt
@@ -0,0 +1,21 @@
+package ipvc.estg.room.dao
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import ipvc.estg.room.entities.City
+
+@Dao
+interface WordDao {
+
+    @Query("SELECT * from city_table ORDER BY city ASC")
+    fun getAlphabetizedWords(): LiveData<List<City>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(city: City)
+
+    @Query("DELETE FROM city_table")
+    suspend fun deleteAll()
+}
