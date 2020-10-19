531a8006e614d28e3b6b5f6fa4db0c34f2cca426
joelvaz99 <joelvaz@ipvc.pt>
2020-10-19 21:33:09 +0100

    Add RecyclerView

--- app/src/main/java/ipvc/estg/room/MainActivity.kt
+++ app/src/main/java/ipvc/estg/room/MainActivity.kt
@@ -2,10 +2,19 @@ package ipvc.estg.room
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import ipvc.estg.room.adapters.CityAdapter
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //recycler view
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerview)
+        val adapter = CityAdapter(this)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
---
+++ app/src/main/java/ipvc/estg/room/adapters/CityAdapter.kt
@@ -0,0 +1,39 @@
+package ipvc.estg.room.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import ipvc.estg.room.R
+import ipvc.estg.room.entities.City
+
+class CityAdapter internal constructor(
+    context: Context
+) : RecyclerView.Adapter<CityAdapter.CityViewHolder>() {
+
+    private val inflater: LayoutInflater = LayoutInflater.from(context)
+    private var cities = emptyList<City>() // Cached copy of words
+
+    inner class CityViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val cityItemView: TextView = itemView.findViewById(R.id.textView)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CityViewHolder {
+        val itemView = inflater.inflate(R.layout.recyclerview_item, parent, false)
+        return CityViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: CityViewHolder, position: Int) {
+        val current = cities[position]
+        holder.cityItemView.text = current.city
+    }
+
+    internal fun setWords(words: List<City>) {
+        this.cities = words
+        notifyDataSetChanged()
+    }
+
+    override fun getItemCount() = cities.size
+}
